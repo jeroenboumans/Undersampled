cbe6e1123dd7d9e89922907ce89bfa32dc0d6ccf
Jeroen Boumans <jeroen.boumans@gmail.com>
2024-01-07 17:33:08 +0100

    Updates

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,6 @@ module.exports = {
         require('postcss-import'),
         require('tailwindcss/nesting'),
         require('tailwindcss'),
-        require('autoprefixer'),
-        ...(process.env.NODE_ENV === 'production'
-            ? [require('@fullhuman/postcss-purgecss')({
-                content: ['./_site/**/*.html'],
-                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-            })
-            ]
-            : [])
+        require('autoprefixer')
     ]
 }
