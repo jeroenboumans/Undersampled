f0c94a64fc75969b3421053ae5885d7bb7b4a919
Jeroen Boumans <jeroen.boumans@gmail.com>
2025-08-10 10:43:26 +0200

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   darkMode: 'class',
   plugins: [
       // require('@tailwindcss/forms')
+      // require('@tailwindcss/aspect-ratio'),
   ],
   content: [
     '_site/**/*.html',
